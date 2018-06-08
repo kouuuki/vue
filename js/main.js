ba562f6fe8c5a1cccd7656cb4bf1e2bbcad26eba
kouuuki <mark.web.888@gmail.com>
2018-06-08 17:39:45 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function(){
+  'use strict';
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      newItem: '',
+      // name: 'taguchi'
+      todos: [
+        'task 1',
+        'task 2',
+        'task 3'
+      ]
+    },
+    methods: {
+      addItem: function() {
+        this.todos.push(this.newItem);
+        this.newItem = '';
+      }
+    }
+  });
+})();
